23f280e586c8cb65d4ac8602ac0c123771b204a4
Administrator <magdalena.lozowska@gmail.com>
2021-05-08 10:59:56 +0200

    initial commit

---
+++ tests/page_objects/checkboxes_page.py
@@ -0,0 +1,27 @@
+from tests.helpers.support_functions import *
+from time import sleep
+
+checkbox_tab = 'checkbox-header'
+all_checkboxes = 'checkboxes'
+checkbox1 = '//*[@id="checkboxes"]/input[1]'
+checkbox2 = '//*[@id="checkboxes"]/input[2]'
+
+
+def checkboxes_visible(driver_instance):
+    elem = wait_for_visibility_of_element(driver_instance, all_checkboxes)
+    return elem.is_displayed()
+
+
+def click_checkboxes_tab(driver_instance):
+    wait_for_visibility_of_element(driver_instance, checkbox_tab)
+    elem = driver_instance.find_element_by_id(checkbox_tab)
+    elem.click()
+
+
+def click_checkboxes(driver_instance):
+    elem = driver_instance.find_element_by_xpath(checkbox1)
+    elem.click()
+    elem1 = driver_instance.find_element_by_xpath(checkbox2)
+    elem1.click()
+    sleep(3)
+
