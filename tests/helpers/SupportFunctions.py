c697b1a46527930d62be328f04c648e9e50083f5
Magda_Lena <60215258+mlozowska@users.noreply.github.com>
2021-05-09 18:24:22 +0200

    Update SupportFunctions.py

--- tests/helpers/SupportFunctions.py
+++ tests/helpers/SupportFunctions.py
@@ -27,13 +27,6 @@ def wait_for_visibility_of_element_id(driver_instance, id):
     return elem
 
 
-# def wait_for_invisibility_of_element_xpath(inv_driver_instance, xpath):
-#     try:
-#         inv_elem = WebDriverWait(inv_driver_instance, 10).until(EC.invisibility_of_element_located((By.XPATH, xpath)))
-#     except TimeoutException:
-#         inv_elem = False
-#     return inv_elem
-
 
 def wait_for_invisibility_of_element_xpath(inv_driver_instance, xpath):
     inv_element = WebDriverWait(inv_driver_instance, 8).until(EC.invisibility_of_element_located((By.XPATH, xpath)))
