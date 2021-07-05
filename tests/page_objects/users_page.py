6c11db4539e18738d00d3fa6b1c4b51fd62df870
Magda_Lena <60215258+mlozowska@users.noreply.github.com>
2021-07-05 22:04:14 +0200

    Delete tests directory

--- tests/page_objects/users_page.py
+++
@@ -1,8 +0,0 @@
-from tests.helpers.SupportFunctions import *
-
-error_info = 'container'
-
-
-def error_info_displayed(driver_instance):
-    elem = wait_for_visibility_of_element_id(driver_instance, error_info)
-    return elem.is_displayed()
